f891e080c77262423a02b7c86c7aadfe9b35118f
durling <878877528@qq.com>
2019-12-23 18:49:42 +0800

    gx

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
-import VConsole from 'vconsole'
+// import VConsole from 'vconsole'
 
 import App from './App.vue'
 import router from './router';
 
-if (!window.location.href.includes('nianran.net') || window.location.href.includes('debug=1')) {
-  new VConsole();
-  window.onerror = (message) => {
-    alert(message)
-  }
-}
+// if (!window.location.href.includes('xiexin.xin') || window.location.href.includes('debug=1')) {
+//   new VConsole();
+//   window.onerror = (message) => {
+//     alert(message)
+//   }
+// }
 
 Vue.config.productionTip = false
 
