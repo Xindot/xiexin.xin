c63dc11adfde25b026febcb44fc44f5a8ef5845d
durling <878877528@qq.com>
2022-04-29 11:28:24 +0800

    gx

--- vue-app/src/components/pages/index/index.vue
+++ vue-app/src/components/pages/index/index.vue
@@ -92,9 +92,11 @@
                 <span class="s-3 float-right clearfix">{{item.duty}}</span>
               </p>
               <p class="p-2">
-                <span class="bold">公司介绍：</span>{{item.companyDesc}}</p>
+                <span class="bold">公司介绍：</span>{{item.companyDesc}}
+              </p>
               <p class="p-2">
-                <span class="bold">工作内容：</span>{{item.dutyDesc}}</p>
+                <span class="bold">工作内容：</span>{{item.dutyDesc}}
+              </p>
             </div>
           </div>
           <!-- 实践经历 -->
@@ -109,7 +111,8 @@
                 <span class="s-3 float-right clearfix">{{item.duty}}</span>
               </p>
               <p class="p-2">
-                <span class="bold">工作内容：</span>{{item.dutyDesc}}</p>
+                <span class="bold">工作内容：</span>{{item.dutyDesc}}
+              </p>
             </div>
           </div>
           <!-- 项目经历 -->
@@ -124,9 +127,11 @@
                 <span class="s-3 float-right clearfix">{{item.duty}}</span>
               </p>
               <p class="p-2">
-                <span class="bold">项目介绍：</span>{{item.projectDesc}}</p>
+                <span class="bold">项目介绍：</span>{{item.projectDesc}}
+              </p>
               <p class="p-2">
-                <span class="bold">工作内容：</span>{{item.dutyDesc}}</p>
+                <span class="bold">工作内容：</span>{{item.dutyDesc}}
+              </p>
               <p v-if="item.remark">
                 <span class="bold">备注说明：</span>{{item.remark}}
               </p>
@@ -168,109 +173,109 @@ export default {
         visible: true,
         htmlTitle: "谢鑫的简历",
         nowTime: TDate,
-        icon: "https://img.6h5.cn/xiexin.xin/pdf.jpeg",
-        doing: false
+        icon: "https://x-cdn.oss-accelerate.aliyuncs.com/img/nran_work/xiexin.xin/img/pdf.jpeg",
+        doing: false,
       },
       blackMode: true, // 夜晚模式
       me: {
-        avatar: "https://img.6h5.cn/xiexin.xin/xin-avatar-2.jpg",
+        avatar:
+          "https://x-cdn.oss-accelerate.aliyuncs.com/img/nran_work/xiexin.xin/img/xin-avatar-2.jpg",
         words: "简单不先于复杂，而是再复杂之后。",
         name: "谢鑫",
         duty: "web前端开发工程师",
         list: [
           {
             label: "姓名",
-            value: "男"
+            value: "男",
           },
           {
             label: "出生年月",
-            value: "1990.10"
+            value: "1990.10",
           },
           {
             label: "毕业学校",
             value: "杭州师范大学",
-            href: "http://www.hznu.edu.cn"
+            href: "http://www.hznu.edu.cn",
           },
           {
             label: "学院专业",
             value: "理学院应用物理学",
-            href: "http://lxy.hznu.edu.cn"
+            href: "http://lxy.hznu.edu.cn",
           },
           {
             label: "手机号",
             value: ["187****5975", "18758885975"][pwd],
-            href: pwd ? "tel:18758885975" : null
+            href: pwd ? "tel:18758885975" : null,
           },
           {
             label: "邮箱",
             value: "xinx@hz.cn",
-            href: "mailto:xinx@hz.cn"
+            href: "mailto:xinx@hz.cn",
           },
           {
             label: "证书",
-            value: "CET-4、计算机二级"
-          }
-        ]
+            value: "CET-4、计算机二级",
+          },
+        ],
       },
       skill: {
         title: " ❀ 专业技能 ❀ ",
         list: [
           {
             label: "➢ HTML",
-            percent: 85
+            percent: 85,
           },
           {
             label: "➢ CSS(SCSS)",
-            percent: 80
+            percent: 80,
           },
           {
             label: "➢ Javascript",
-            percent: 75
+            percent: 75,
           },
           {
             label: "➢ Vue.js",
-            percent: 80
+            percent: 80,
           },
           {
             label: "➢ React",
-            percent: 50
+            percent: 50,
           },
           {
             label: "➢ Node.js",
-            percent: 70
-          }
-        ]
+            percent: 70,
+          },
+        ],
       },
       interest: {
         title: " ❀ 兴趣爱好 ❀ ",
-        desc:
-          "喜欢书法、读书，游泳、爬山、平时写写技术博客，也搭建了自定义的个人博客：<a href='http://xindot.com' target='_blank'>xindot.com</a>"
+        desc: "喜欢书法、读书，游泳、爬山、平时写写技术博客，也搭建了自定义的个人博客：<a href='http://xindot.com' target='_blank'>xindot.com</a>",
       },
       eResume: {
         visible: true,
         title: " ❀ 电子简历 ❀ ",
-        img: "https://img.6h5.cn/xiexin.xin/1577155712.png"
+        img: "https://x-cdn.oss-accelerate.aliyuncs.com/img/nran_work/xiexin.xin/img/1577155712.png",
       },
       intention: {
         title: " ❀ 求职意向",
         list: [
           {
             label: "岗位",
-            value: "web前端开发工程师"
+            value: "web前端开发工程师",
           },
           {
             label: "期望薪资",
-            value: ["****", "20k - 25k"][pwd]
+            value: ["****", "20k - 25k"][pwd],
           },
           {
             label: "地点",
-            value: "杭州"
+            value: "杭州",
           },
           {
             label: "工作性质",
-            value: "全职"
-          }
-        ]
+            value: "全职",
+          },
+        ],
       },
       edu: {
         title: " ❀ 教育背景",
@@ -278,19 +283,19 @@ export default {
           {
             time: "2010.9 - 2014.6",
             school: "杭州师范大学理学院",
-            major: "应用物理学"
+            major: "应用物理学",
           },
           {
             time: "2009.9 - 2010.6",
             school: "信阳市商城高中",
-            major: "理科"
+            major: "理科",
           },
           {
             time: "2006.9 - 2009.6",
             school: "信阳市商城达权店高中",
-            major: "理科"
-          }
-        ]
+            major: "理科",
+          },
+        ],
       },
       work: {
         title: " ❀ 工作经历",
@@ -302,7 +307,7 @@ export default {
             companyDesc:
               "是一家为工程机械行业提供服务的物联网+互联网公司，公司产品主要包含云盒硬件和APP、SaaS等软件。",
             dutyDesc:
-              "主要负责企业官网、APP内h5页面、配件商城全栈、管理后台若干、微信支付宝支付及退款、七牛文件操作、为业务稳定提供部分接口、小程序等项目；主要技术用到了es6、vue、react、node、webpack、nginx、mysql、sequelize、uni-app；使用了VSCode、HBuilderX、微信开发、支付宝小程序开发、Termius等工具。"
+              "主要负责企业官网、APP内h5页面、配件商城全栈、管理后台若干、微信支付宝支付及退款、七牛文件操作、为业务稳定提供部分接口、小程序等项目；主要技术用到了es6、vue、react、node、webpack、nginx、mysql、sequelize、uni-app；使用了VSCode、HBuilderX、微信开发、支付宝小程序开发、Termius等工具。",
           },
           {
             time: "2015.10 - 2017.2",
@@ -311,7 +316,7 @@ export default {
             companyDesc:
               "是一家境外旅游+户外直播的互联网公司。主要产品是APP和PC管理端。",
             dutyDesc:
-              "主要负责旅游项目官网、运营管理后台、作者管理后台、翻译后台、webviewH5下单流程、微信授权话题活动等项目；主要技术包含Nodejs，Avalon，Vue，git，redis；开发工具包含了Sublime、Atom、Transmit。"
+              "主要负责旅游项目官网、运营管理后台、作者管理后台、翻译后台、webviewH5下单流程、微信授权话题活动等项目；主要技术包含Nodejs，Avalon，Vue，git，redis；开发工具包含了Sublime、Atom、Transmit。",
           },
           {
             time: "2014.6 - 2015.10",
@@ -320,9 +325,9 @@ export default {
             companyDesc:
               "是一家农产品开发+互联网的生鲜公司，主要产品是pc网站和门店。",
             dutyDesc:
-              "主要负责ecshop模板搭建，主要页面的二次开发，包括首页重新布局、活动页面制作、配合php同事完善后台一些基本功能页面的实现；主要技术包含：html(H5)、css(3)、JS、jQuery、Bootstrap、git；编辑工具主要使用Sublime。"
-          }
-        ]
+              "主要负责ecshop模板搭建，主要页面的二次开发，包括首页重新布局、活动页面制作、配合php同事完善后台一些基本功能页面的实现；主要技术包含：html(H5)、css(3)、JS、jQuery、Bootstrap、git；编辑工具主要使用Sublime。",
+          },
+        ],
       },
       practice: {
         title: " ❀ 实践经历",
@@ -333,9 +338,9 @@ export default {
             duty: "web前端开发",
             projectDesc: "",
             dutyDesc:
-              "主要负责淘宝店装修、模板网站搭建装修等；技术主要是html、css、js基础、PhotoShop基础。"
-          }
-        ]
+              "主要负责淘宝店装修、模板网站搭建装修等；技术主要是html、css、js基础、PhotoShop基础。",
+          },
+        ],
       },
       project: {
         title: " ❀ 项目经历",
@@ -346,7 +351,7 @@ export default {
             duty: "web前端",
             projectDesc: "为加油商及加油客户提供金融解决方案",
             dutyDesc:
-              "主要使用react + ant.design + react-router搭建管理后台，其中使用umi对项目进行本地运行和打包。"
+              "主要使用react + ant.design + react-router搭建管理后台，其中使用umi对项目进行本地运行和打包。",
           },
           {
             time: "2020.4 - 2020.8",
@@ -355,14 +360,14 @@ export default {
             projectDesc: "为奶茶总部提供一整套点单加盟解决方案",
             dutyDesc:
               "实现微信点单小程序，主要功能点:微信下单支付、订阅推送、各种优惠券的使用、云打印机对接等功能；实现总部管理后台：用户统计、订单统计、结算统计、门店统计、销量统计等，实现订单处理、加盟处理、门店管理等系统功能；实现进货系统：进货商申请、进货单处理等。",
-            remark: "周末时间完成"
+            remark: "周末时间完成",
           },
           {
             time: "2019.11 - 2020.3",
             project: "智慧工地项目：pc管理后台",
             duty: "web前端",
             projectDesc: "为工地管理方提供一站式软硬件服务",
-            dutyDesc: "vue-cli3 + element-ui + vue-router + axios"
+            dutyDesc: "vue-cli3 + element-ui + vue-router + axios",
           },
           {
             time: "2019.7 - 2019.12",
@@ -370,7 +375,7 @@ export default {
             duty: "web前端",
             projectDesc: "非移动机械信息申报和使用管理",
             dutyDesc:
-              "主要负责非道微信、支付宝（兼容浙里办）小程序的前端开发和管理后台的开发，小程序主要运用了uni-app框架和支付宝小程序框架，管理后台主要是vue-cli3脚手架+element-ui库、其中使用了axios（request封装）、vue-router、高德地图组件、echarts图表库、二维码生成库等；js库使用cdn、使用rsync优化了发布流程、使用polyfill兼容部分浏览器。"
+              "主要负责非道微信、支付宝（兼容浙里办）小程序的前端开发和管理后台的开发，小程序主要运用了uni-app框架和支付宝小程序框架，管理后台主要是vue-cli3脚手架+element-ui库、其中使用了axios（request封装）、vue-router、高德地图组件、echarts图表库、二维码生成库等；js库使用cdn、使用rsync优化了发布流程、使用polyfill兼容部分浏览器。",
           },
           {
             time: "2019.6 - 2019.8",
@@ -379,7 +384,7 @@ export default {
             projectDesc:
               "提供油卡充值、话费充值、附近加油站、违章查询、商城功能。",
             dutyDesc:
-              "主要在微信端使用h5实现app端所有内容，并给app端提供部分webview页面；主要实现的技术点：扩展vue-router使跳转页面不用关系路径问题；使用vue监听路由让页面跳转有过渡效果；商品分类详情页交互、微信支付交互等各种体验的优化。"
+              "主要在微信端使用h5实现app端所有内容，并给app端提供部分webview页面；主要实现的技术点：扩展vue-router使跳转页面不用关系路径问题；使用vue监听路由让页面跳转有过渡效果；商品分类详情页交互、微信支付交互等各种体验的优化。",
           },
           {
             time: "2018.10 - 2019.5",
@@ -387,7 +392,7 @@ export default {
             duty: "web前端+Node",
             projectDesc: "为公司部分项目提供部分基础接口",
             dutyDesc:
-              "使用 nodejs + express/koa2 + mysql + jwt 实现部分基础接口：为APP客户端提供七牛云/阿里OSS上传图片的uptoken；接口验证；微信分享配置接口；excel导出工具；页面埋点日志接口；系统报错邮件提醒等。"
+              "使用 nodejs + express/koa2 + mysql + jwt 实现部分基础接口：为APP客户端提供七牛云/阿里OSS上传图片的uptoken；接口验证；微信分享配置接口；excel导出工具；页面埋点日志接口；系统报错邮件提醒等。",
           },
           {
             time: "2018.5 - 2018.10",
@@ -395,7 +400,7 @@ export default {
             duty: "web前端",
             projectDesc: "为公司提供管理贷款信息平台",
             dutyDesc:
-              "主要使用react + ant.design + react-router搭建管理后台，其中使用umi对项目进行本地运行和打包。"
+              "主要使用react + ant.design + react-router搭建管理后台，其中使用umi对项目进行本地运行和打包。",
           },
           {
             time: "2018.3 - 2018.6",
@@ -403,7 +408,7 @@ export default {
             duty: "web前端+Node",
             projectDesc: "为公司提供一整套商城系统服务",
             dutyDesc:
-              "接口nodejs + express + mysql；h5端使用vue ssr服务端渲染；管理后台vuejs + webpack3；主要难点：微信支付宝支付验签、订单部分退款等。"
+              "接口nodejs + express + mysql；h5端使用vue ssr服务端渲染；管理后台vuejs + webpack3；主要难点：微信支付宝支付验签、订单部分退款等。",
           },
           {
             time: "2017.10 - 2018.3",
@@ -411,16 +416,16 @@ export default {
             duty: "web前端+Node",
             projectDesc: "用户参与h5抽奖活动并参与聊天讨论",
             dutyDesc:
-              "主要使用nodejs + websocket + mysql；h5端使用vue ssr服务端渲染；socket实现消息主动推送。"
-          }
-        ]
+              "主要使用nodejs + websocket + mysql；h5端使用vue ssr服务端渲染；socket实现消息主动推送。",
+          },
+        ],
       },
       awards: {
         title: " ❀ 获奖经历",
         list: [
           "➢ 2011年度第八届全国残疾人运动会优秀志愿者",
-          "➢ 2013年度中国大学生自强之星提名奖"
-        ]
+          "➢ 2013年度中国大学生自强之星提名奖",
+        ],
       },
       assessment: {
         title: " ❀ 自我评价",
@@ -428,9 +433,9 @@ export default {
           "➢ 具有良好的语言表达和沟通能力，拥有良好的团队协作精神和较强的时间意识，工作主动认真，诚实守信，具有较强的责任感。善于学习，肯花时间，不埋怨。",
           "➢ 具有较强的学习和自学新知识的能力，热爱IT行业。",
           "➢ 有较好的团队协调能力，无论对同事还是对学生，都是一个很好的助手。",
-          "➢ 热心公益，个人事迹曾被<a href='http://qjwb.zjol.com.cn/html/2013-04/11/content_2087686.htm?div=-1' target='_blank'>钱江晚报</a>报道过。"
-        ]
-      }
+          "➢ 热心公益，个人事迹曾被<a href='http://qjwb.zjol.com.cn/html/2013-04/11/content_2087686.htm?div=-1' target='_blank'>钱江晚报</a>报道过。",
+        ],
+      },
     };
   },
   created() {
@@ -473,8 +478,8 @@ export default {
       // // console.log(img);
       // this.eResume.img = img;
       // this.eResume.visible = true;
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
